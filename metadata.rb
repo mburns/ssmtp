9667ea1802c2a809472920166d75169645e0460d
Sander van Zoest <svanzoest@onehealth.com>
2013-07-08 13:50:26 -0700

    version bump (0.3.0)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "sander@vanzoest.com"
 license          "Apache 2.0"
 description      "Installs/Configures ssmtp"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.3.0"
 supports         "debian"
 supports         "ubuntu"
 supports         "centos"
