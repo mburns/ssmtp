a91a13672bf5078dd58c17e6caacc925ea214bd7
Michael Burns <michael@mirwin.net>
2018-02-19 23:58:17 -0800

    fix metadata, bump version

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,14 @@
 name 'ssmtp'
 maintainer 'Sander van Zoest'
 maintainer_email 'sander+cookbooks@vanzoest.com'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs/Configures ssmtp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url 'https://github.com/svanzoest-cookbooks/ssmtp/issues'
 source_url 'https://github.com/svanzoest-cookbooks/ssmtp/'
-version '0.4.4'
+version '0.5.0'
 supports 'debian'
 supports 'ubuntu'
 supports 'centos'
 depends 'yum-epel'
+chef_version '>= 12' if respond_to?(:chef_version)
