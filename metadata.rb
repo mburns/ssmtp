b79d5f255f5d5ca2121ef2e1ed49eca3f8f48a28
Michael Burns <michael@mirwin.net>
2018-02-19 23:52:26 -0800

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures ssmtp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url 'https://github.com/svanzoest-cookbooks/ssmtp/issues'
 source_url 'https://github.com/svanzoest-cookbooks/ssmtp/'
-version '0.4.3'
+version '0.4.4'
 supports 'debian'
 supports 'ubuntu'
 supports 'centos'
